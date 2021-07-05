8a4a815dd98cc52972a786875a26fe51bd70d1cc
Bardhi <fatbardh.kadriu>
2021-07-05 19:09:58 +0200

    create/get posts and added some style

--- server/index.js
+++ server/index.js
@@ -3,14 +3,23 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
 
-class className  {
+const app = express();
 
-    constructor(name) {
-        this.name=name;
+app.use('/posts', postRoutes);
 
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-    }
-}
+const CONNECTION_URL = "mongodb+srv://fatbardh:bardhidb@cluster0.m9htn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
 
 
