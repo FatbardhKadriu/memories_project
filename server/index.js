23d2b8b6f50d5c74af5d83249847c2cb6ff6b15a
Bardhi <fatbardh.kadriu>
2021-07-05 18:23:38 +0200

    Set up project / 2

--- server/index.js
+++ server/index.js
@@ -2,3 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+
+class className  {
+
+    constructor(name) {
+        this.name=name;
+
+
+    }
+}
+
+
