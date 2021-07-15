7ce96d6b90068fefef5a815b9e93824e4ad6fd11
Fatbardh <fatbardh@gmail.com>
2021-07-15 14:41:05 +0200

    Styling

--- server/index.js
+++ server/index.js
@@ -2,8 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js';
+
+dotenv.config()
+
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
 
 const app = express();
 
@@ -13,7 +20,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://fatbardh:bardhidb@cluster0.m9htn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWD}@cluster0.m9htn.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
