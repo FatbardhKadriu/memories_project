00cc69458563a04a5eeedb2e0d5fa6abd80f6069
Fatbardh <fatbardh@gmail.com>
2021-07-16 11:19:48 +0200

    Environment variables example

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWD}@cluster0.m9htn.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
