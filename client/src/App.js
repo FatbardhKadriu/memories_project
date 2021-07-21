de513d86bcd67da00b272dbfecef5316fb200e2c
Fatbardh <fatbardh@gmail.com>
2021-07-21 13:46:26 +0200

    Minor fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Container} from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 import Auth from './components/Auth/Auth';
 
 const App = () => (
-    <BrowserRouter>
-    <Container maxwidth="lg">
-        <Navbar />
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/auth" exact component={Auth} />
-        </Switch>
+  <BrowserRouter>
+      <Container maxWidth="lg">
+          <Navbar />
+          <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/auth" exact component={Auth} />
+          </Switch>
     </Container>
-    </BrowserRouter>
+  </BrowserRouter>
 );
 
 export default App;
